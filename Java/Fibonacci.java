ddbe0a73a28d6cf6244957ea9dea8e72ec24cf2a
Pradyumna Bhat <122141906+pradyumna100903@users.noreply.github.com>
2025-10-24 18:59:21 +0530

    Added Fibonacci Sequence Program in Java

---
+++ Java/Fibonacci.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+
+class Fibonacci{  
+public static void main(String args[])  
+{    
+ int n1=0,n2=1,n3,i;
+  Scanner s=new Scanner(System.in);
+  System.out.println("Enter The Number of Terms You want to Print ");
+  int count=s.nextInt();
+ System.out.print(n1+" "+n2);//printing 0 and 1    
+    
+ for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
+ {    
+  n3=n1+n2;    
+  System.out.print(" "+n3);    
+  n1=n2;    
+  n2=n3;    
+ }    
+  
+ }
+}  
